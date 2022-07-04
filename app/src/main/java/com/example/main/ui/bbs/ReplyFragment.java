79ace855a1717f353ade3cc59b16eb7f9128bcbd
osarusan <yuta170@ymobile.ne.jp>
2022-07-04 12:04:39 +0900

    fixCode

--- app/src/main/java/com/example/main/ui/bbs/ReplyFragment.java
+++ app/src/main/java/com/example/main/ui/bbs/ReplyFragment.java
@@ -1,7 +1,5 @@
 package com.example.main.ui.bbs;
 
-import static android.content.ContentValues.TAG;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -25,6 +23,8 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
+import static android.content.ContentValues.TAG;
+
 public class ReplyFragment extends Fragment {
 
     ArrayList<String> data = new ArrayList<>();
@@ -32,6 +32,7 @@ public class ReplyFragment extends Fragment {
 
     FirebaseDatabase db = FirebaseDatabase.getInstance();
     DatabaseReference reference = db.getReference();
+    DatabaseReference replyReference = db.getReference();
 
     //xmlとの紐づけ用の変数
     private EditText commentEditText;
@@ -45,6 +46,8 @@ public class ReplyFragment extends Fragment {
 
         String firebaseKey = getArguments().getString("FirebaseKey");
         String title = getArguments().getString("Title");
+        //読込先の場所を参照する
+        replyReference = db.getReference("Threads").child("Thread").child(firebaseKey).child("reply");
 
         //遷移元のフラグメントから情報を受け取る処理
 //        Intent intent = getIntent();
@@ -54,98 +57,94 @@ public class ReplyFragment extends Fragment {
         TextView textView3 = root.findViewById(R.id.textVIew3);
         textView3.setText(title);
 
-//もどるボタンの処理
-        root.findViewById(R.id.back_button).setOnClickListener(
-                view -> {
-                    Fragment back = new BbsFragment();
-                    FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
-                    transaction.replace(R.id.nav_host_fragment, back);
-                    transaction.addToBackStack(null);
-                    transaction.commit();
-                });
+        //リストビューとの紐づけ
+        ListView mListView = root.findViewById(R.id.list_view2);
 
-//返信ボタンの処理
+        //CustomAdapterをセットする
+        mReplyCustomAdapter = new ReplyCustomAdapter(requireActivity().getApplicationContext(), R.layout.card_view, new ArrayList<ReplyData>());
+        mListView.setAdapter(mReplyCustomAdapter);
+
+        //もどるボタンの処理
+        root.findViewById(R.id.back_button).setOnClickListener(view -> {
+            Fragment back = new BbsFragment();
+            FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+            transaction.replace(R.id.nav_host_fragment, back);
+            transaction.addToBackStack(null);
+            transaction.commit();
+        });
+
+
+        //返信ボタンの処理
         commentEditText = root.findViewById(R.id.comment);
 
-        root.findViewById(R.id.add_button2).setOnClickListener(
-                view -> {
+        root.findViewById(R.id.add_button2).setOnClickListener(view -> {
+            String comment = commentEditText.getText().toString();
+            String key = reference.push().getKey();
 
-                    String comment = commentEditText.getText().toString();
-                    String key = reference.push().getKey();
+            //入力欄どちらかがからだったら何もしない
+            if (comment.isEmpty()) {
+                return;
+            }
+            ReplyData replyData = new ReplyData(key, comment);
 
-                    //入力欄どちらかがからだったら何もしない
-                    if (comment.isEmpty()) {
-                        return;
-                    }
-                    ReplyData replyData = new ReplyData(key, comment);
+            //Threadsに一意のIDをつけて投稿内容を保存する
+            reference.child("Threads").child("Thread").child(firebaseKey).child("reply").child(key).setValue(replyData);
 
-                    //Threadsに一意のIDをつけて投稿内容を保存する
-                    reference.child("Threads").child("Thread").child(firebaseKey).child("reply").child(key).setValue(replyData);
+            //RealtimeDatabaseのインスタンスを取得
+            db = FirebaseDatabase.getInstance();
 
-                    //RealtimeDatabaseのインスタンスを取得
-                    db = FirebaseDatabase.getInstance();
+            DatabaseReference pushedThreadsRef = db.getReference().push();
+            String threadsId = pushedThreadsRef.getKey();
 
-                    DatabaseReference pushedThreadsRef = db.getReference().push();
-                    String threadsId = pushedThreadsRef.getKey();
-
-                    Log.d(TAG, "onCreateView: " + threadsId);
+            reference = db.getReference();
+        });
 
 
-                    //書き込み先の場所を参照する
-                    reference = db.getReference("Threads").child("Thread").child(firebaseKey).child("reply");
+        //firebaseと同期するリスナー
+        replyReference.addChildEventListener(new ChildEventListener() {
 
-                    //リストビューとの紐づけ
-                    ListView mListView = root.findViewById(R.id.list_view2);
+            //アイテムのリストを取得するか、アイテムのリストへの追加がないかを監視する
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                //コールバック関数はDataSnapshot
+                //スナップショットとは、ある時点における特定のデータベースにあるデータの全体像を写し取ったもの
+                //スナップショットのgetValue()を呼び出すと、データの言語固有のオブジェクト表現が返される
+                //参照先の場所にデータが存在しない場合は、値がnullになる
 
-                    //CustomAdapterをセットする
-                    mReplyCustomAdapter = new ReplyCustomAdapter(requireActivity().getApplicationContext(), R.layout.card_view, new ArrayList<ReplyData>());
-                    mListView.setAdapter(mReplyCustomAdapter);
+                Log.d(TAG, "onChildAdded: " + snapshot.getValue().getClass());
 
-                    //firebaseと同期するリスナー
-                    reference.addChildEventListener(new ChildEventListener() {
-                        //アイテムのリストを取得するか、アイテムのリストへの追加がないかを監視する
-                        @Override
-                        public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                            //コールバック関数はDataSnapshot
-                            //スナップショットとは、ある時点における特定のデータベースにあるデータの全体像を写し取ったもの
-                            //スナップショットのgetValue()を呼び出すと、データの言語固有のオブジェクト表現が返される
-                            //参照先の場所にデータが存在しない場合は、値がnullになる
+                //BBSDataクラスで指定した形式にする
+                ReplyData replyData = snapshot.getValue(ReplyData.class);
+                //上の行で得た値を、CustomAdapterに挿入する
+                mReplyCustomAdapter.add(replyData);
+                //CustomAdapterの変更を画面に反映させる
+                mReplyCustomAdapter.notifyDataSetChanged();
+            }
 
-                            Log.d(TAG, "onChildAdded: " + snapshot.getValue().getClass());
+            //リスト内にアイテムに対する変更がないかを監視する
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
 
-                            //BBSDataクラスで指定した形式にする
-                            ReplyData replyData = snapshot.getValue(ReplyData.class);
-                            //上の行で得た値を、CustomAdapterに挿入する
-                            mReplyCustomAdapter.add(replyData);
-                            //CustomAdapterの変更を画面に反映させる
-                            mReplyCustomAdapter.notifyDataSetChanged();
-                        }
+            }
 
-                        //リスト内にアイテムに対する変更がないかを監視する
-                        @Override
-                        public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            //リストから削除されるアイテムがないかを監視する
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
 
-                        }
+            }
 
-                        //リストから削除されるアイテムがないかを監視する
-                        @Override
-                        public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+            //並べ替えリストの項目順に変更がないかを監視する
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
 
-                        }
+            }
 
-                        //並べ替えリストの項目順に変更がないかを監視する
-                        @Override
-                        public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            //ログを記録するなどError時の処理を記載する
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
 
-                        }
-
-                        //ログを記録するなどError時の処理を記載する
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError error) {
-
-                        }
-                    });
-                });
+            }
+        });
 
         return root;
     }
